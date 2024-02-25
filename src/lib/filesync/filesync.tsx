cd3961932d756542b01d7d5d0ed50dd0e4f408dc
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-02-25 14:51:14 +0900

    :zap: ダイナミックルーティングが難しい

---
+++ src/lib/filesync/filesync.tsx
@@ -0,0 +1,8 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function Filesync() {
+  const dummyPath = path.join(' ');
+
+  return <p>filesyncの動きを調べる</p>;
+}
---
+++ src/lib/posts/posts.tsx
@@ -0,0 +1,8 @@
+import fetch from 'node-fetch';
+
+export async function gerAllPostsIds() {
+  const youtubeApiPath = 'https://www.googleapis.com/youtube/v3/search';
+  const response = await fetch(youtubeApiPath);
+  const data = await response.json();
+  console.log(data);
+}
