35bc247c86b2bc96b36a4f69bdf82d76606b7d05
Hiroya Nakano <hiroya_0610_15mro@me.com>
2024-03-20 16:08:55 +0900

    :zap: ニュースページ作成

--- src/components/elements/panel/panel.tsx
+++ src/components/elements/panel/panel.tsx
@@ -1,7 +1,13 @@
 'use client';
 
-import Link from 'next';
 import { styled } from 'styled-components';
+
+/**
+ * JSONから返却される記事データの型
+ */
+type ArticlePanelProps = {
+  articles: Feed[];
+};
 
 /**
  * JSONから返却される記事データの型
@@ -28,21 +34,121 @@ type Sentiment = {
   ticker_sentiment_label: string;
   ticker_sentiment_score: string;
 };
+
 type Topics = {
   relevance_score: string;
   score: string;
 };
 
 const Panel = styled.div`
-  border: 1px solid #eb6299;
-  border-radius: 5px;
   margin-top: 20px;
 `;
 
-export const ArticlePanel = (props: Feed[]) => {
+const Article = styled.div`
+  overflow: hidden;
+  background: rgba(255, 255, 255, 0.25); /* カード背景の色＋不透明度 */
+  backdrop-filter: blur(10px); /* 背景のぼかし具合 */
+  border-radius: 16px; /* 角の丸み */
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); /* 外側の影 */
+  border: 1px solid rgba(255, 255, 255, 0.3); /* 縁のスタイル */
+
+  + div {
+    margin-top: 30px;
+  }
+`;
+
+const Img = styled.img`
+  aspect-ratio: 700 / 400;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const Anchor = styled.a`
+  display: grid;
+  transition: opacity 0.2s ease-out;
+  gap: 10px;
+
+  @media screen and (min-width: 760px) {
+    gap: 20px;
+    grid-template-areas:
+      'Image Title'
+      'Image Published';
+    grid-template-rows: 300px 1fr;
+    grid-template-columns: 300px 1fr;
+  }
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const Item1 = styled.div`
+  order: 1;
+
+  @media screen and (min-width: 760px) {
+    grid-area: 'Image';
+  }
+`;
+
+const Item2 = styled.div`
+  order: 2;
+
+  @media screen and (min-width: 760px) {
+    grid-area: 'Title';
+    font-size: 2rem;
+  }
+`;
+
+const Item3 = styled.time`
+  order: 3;
+
+  @media screen and (min-width: 760px) {
+    grid-area: 'Published';
+    order: none;
+  }
+`;
+
+export const ArticlePanel = ({ articles }: ArticlePanelProps) => {
+  console.log(articles);
+  const articlePanelItems = articles.map((article, index) => {
+    const dateString = article.time_published.split('T')[0];
+    const yyyy = dateString.substring(0, 4);
+    const mm = dateString.substring(4, 6);
+    const dd = dateString.substring(6, 8);
+    const kebabCase = `${yyyy}-${mm}-${dd}`;
+
+    const dateObject = new Date(kebabCase);
+    const year = dateObject.getFullYear();
+    const month = dateObject.getMonth() + 1;
+    const day = dateObject.getDate();
+    const formattedDate = `${year}年${month}月${day}日`;
+    const dateTimeValue = `${year}-${month}-${day}`;
+    console.log(article.time_published.split('T')[0]);
+
+    return (
+      <Article
+        role='article'
+        aria-labelledby={`article-name${index}`}
+        aria-describedby={`article-summary${index} article-date${index} article-author${index}`}
+        key={article.title}
+      >
+        <Anchor href={article.url}>
+          <Item2 id={`article-name${index}`}>{article.title}</Item2>
+          <Item3 dateTime={dateTimeValue} id={`article-date${index}`}>
+            {formattedDate}
+          </Item3>
+          <Item1>
+            <Img src={article.banner_image} alt='' />
+          </Item1>
+        </Anchor>
+      </Article>
+    );
+  });
+
   return (
-    <Panel>
-      <Link></Link>
-    </Panel>
+    <>
+      <Panel>{articlePanelItems}</Panel>
+    </>
   );
 };
